d44b91e2f05c22a524241e50e1af0fb0b15a580f
Valery Basova <basovavalery@gmail.com>
2022-09-02 18:22:29 -0400

    add recycelrView and Listener

--- app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsFragment.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsFragment.kt
@@ -5,10 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.android.politicalpreparedness.database.ElectionDatabase
 import com.example.android.politicalpreparedness.databinding.FragmentElectionBinding
+import com.example.android.politicalpreparedness.election.adapter.ElectionListAdapter
+import com.example.android.politicalpreparedness.election.adapter.ElectionListener
 import com.example.android.politicalpreparedness.repository.ElectionsRepository
 
 class ElectionsFragment : Fragment() {
@@ -16,12 +19,15 @@ class ElectionsFragment : Fragment() {
     private lateinit var database: ElectionDatabase
     private lateinit var repository: ElectionsRepository
     private val viewModel: ElectionsViewModel by lazy {
-                val activity = requireNotNull(this.activity) {
-                    "You can only access the viewModel after onViewCreated()"
-                }
-                ViewModelProvider(this, ElectionsViewModelFactory(repository)).get(ElectionsViewModel::class.java)
+        val activity = requireNotNull(this.activity) {
+            "You can only access the viewModel after onViewCreated()"
+        }
+        ViewModelProvider(
+            this,
+            ElectionsViewModelFactory(repository)
+        ).get(ElectionsViewModel::class.java)
 
-            }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,14 +43,21 @@ class ElectionsFragment : Fragment() {
 
         binding.viewModel = viewModel
 
-        //TODO: Add binding values
+        val adapter = ElectionListAdapter((ElectionListener {
+            findNavController().navigate(
+                ElectionsFragmentDirections.actionElectionsFragmentToVoterInfoFragment(
+                    it.id,
+                    it.division
+                )
+            )
+        }))
 
-        //TODO: Link elections to voter info
-
-        //TODO: Initiate recycler adapters
-
-        //TODO: Populate recycler adapters
-
+        binding.electionsList.adapter = adapter
+        viewModel.upcomingElections.observe(viewLifecycleOwner, Observer {
+            it.let {
+                adapter.submitList(it)
+            }
+        })
         return binding.root
     }
 
--- app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsViewModel.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsViewModel.kt
@@ -1,12 +1,19 @@
 package com.example.android.politicalpreparedness.election
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.android.politicalpreparedness.network.models.Election
 import com.example.android.politicalpreparedness.repository.ElectionsRepository
 
 //TODO: Construct ViewModel and provide election datasource
-class ElectionsViewModel(private val electionsRepository: ElectionsRepository): ViewModel() {
+class ElectionsViewModel(private val electionsRepository: ElectionsRepository) : ViewModel() {
 
     //TODO: Create live data val for upcoming elections
+
+    private val _upcomingElections = MutableLiveData<List<Election>>()
+    val upcomingElections: LiveData<List<Election>>
+        get() = _upcomingElections
 
     //TODO: Create live data val for saved elections
 
