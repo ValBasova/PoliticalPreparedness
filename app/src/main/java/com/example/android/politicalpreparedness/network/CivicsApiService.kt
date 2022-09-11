9ce63f890852a6421a0f37c918c23e9d9582f5f1
Valery Basova <basovavalery@gmail.com>
2022-09-11 16:24:20 -0400

    set string array to spinner

--- app/src/main/java/com/example/android/politicalpreparedness/network/CivicsApiService.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/network/CivicsApiService.kt
@@ -2,6 +2,7 @@ package com.example.android.politicalpreparedness.network
 
 import com.example.android.politicalpreparedness.network.jsonadapter.ElectionAdapter
 import com.example.android.politicalpreparedness.network.models.ElectionResponse
+import com.example.android.politicalpreparedness.network.models.RepresentativeResponse
 import com.example.android.politicalpreparedness.network.models.VoterInfoResponse
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
@@ -42,9 +43,8 @@ interface CivicsApiService {
     suspend fun getVoterInfo( @Query("address") address: String
                               , @Query("electionId") electionId: Int) : VoterInfoResponse
 
-    //TODO: Add representatives API Call
     @GET("representatives")
-    suspend fun getRepresentatives(@Query("address") address: String = "1600 Amphitheatre Pkwy, Mountain View, CA 94043, United States"): Call<String>
+    suspend fun getRepresentatives(@Query("address") address: String ): RepresentativeResponse
 }
 
 object CivicsApi {
