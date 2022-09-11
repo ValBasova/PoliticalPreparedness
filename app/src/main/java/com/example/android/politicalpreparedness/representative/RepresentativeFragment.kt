f99d030bdce27668a244989e222e1758cd4aee6d
Valery Basova <basovavalery@gmail.com>
2022-09-11 19:11:42 -0400

    add Location Permissions

--- app/src/main/java/com/example/android/politicalpreparedness/representative/RepresentativeFragment.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/representative/RepresentativeFragment.kt
@@ -1,29 +1,36 @@
 package com.example.android.politicalpreparedness.representative
 
-import android.content.Context
-import android.location.Geocoder
+import android.Manifest
+import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
-import android.view.*
-import android.view.inputmethod.InputMethodManager
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.android.politicalpreparedness.R
 import com.example.android.politicalpreparedness.databinding.FragmentRepresentativeBinding
-import com.example.android.politicalpreparedness.election.ElectionsViewModel
-import com.example.android.politicalpreparedness.network.models.Address
-import java.util.Locale
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class DetailFragment : Fragment() {
 
     companion object {
-        //TODO: Add Constant for Location request
+        private val REQUEST_LOCATION_PERMISSION = 1
     }
 
     private lateinit var viewModel: RepresentativeViewModel
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
-    override fun onCreateView(inflater: LayoutInflater,
-                              container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
 
         val binding = FragmentRepresentativeBinding.inflate(inflater)
 
@@ -33,43 +40,86 @@ class DetailFragment : Fragment() {
 
         //TODO: Populate Representative adapter
 
-        //TODO: Establish button listeners for field and location search
+        //TODO: Establish button listeners for field
+
+        fusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(requireContext())
+
+        binding.buttonLocation.setOnClickListener {
+            if (checkLocationPermissions()) {
+                getLocation()
+            }
+        }
 
         return binding.root
     }
 
-//    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-//        //TODO: Handle location permission result to get location on permission granted
-//    }
-//
-//    private fun checkLocationPermissions(): Boolean {
-//        return if (isPermissionGranted()) {
-//            true
-//        } else {
-//            //TODO: Request Location permissions
-//            false
-//        }
-//    }
-//
-//    private fun isPermissionGranted() : Boolean {
-//        //TODO: Check if permission is already granted and return (true = granted, false = denied/other)
-//    }
-//
-//    private fun getLocation() {
-//        //TODO: Get location from LocationServices
-//        //TODO: The geoCodeLocation method is a helper function to change the lat/long location to a human readable street address
-//    }
-//
+    private fun checkLocationPermissions(): Boolean {
+        return if (isPermissionGranted()) {
+            true
+        } else {
+            requestPermissions(
+                arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_LOCATION_PERMISSION
+            )
+            false
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            if (grantResults.size > 0 && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                getLocation()
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    getString(R.string.permission_denied_explanation),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    private fun isPermissionGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) === PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun getLocation() {
+        fusedLocationProviderClient.lastLocation
+            .addOnSuccessListener { location: Location? ->
+                if (location != null) {
+                    val lat = location.latitude
+                    val long = location.longitude
+                    Log.d("MY LOCATION", lat.toString() + " " + long)
+                }
+            }
+        //TODO: The geoCodeLocation method is a helper function to change the lat/long location to a human readable street address
+
+    }
+
 //    private fun geoCodeLocation(location: Location): Address {
 //        val geocoder = Geocoder(context, Locale.getDefault())
 //        return geocoder.getFromLocation(location.latitude, location.longitude, 1)
-//                .map { address ->
-//                    Address(address.thoroughfare, address.subThoroughfare, address.locality, address.adminArea, address.postalCode)
-//                }
-//                .first()
+//            .map { address ->
+//                Address(
+//                    address.thoroughfare,
+//                    address.subThoroughfare,
+//                    address.locality,
+//                    address.adminArea,
+//                    address.postalCode
+//                )
+//            }
+//            .first()
 //    }
-//
+
 //    private fun hideKeyboard() {
 //        val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
 //        imm.hideSoftInputFromWindow(view!!.windowToken, 0)
