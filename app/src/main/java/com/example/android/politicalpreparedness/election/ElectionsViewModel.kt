55812f902a3a66fda8d9ab140bb18fcfa29988ec
Valery Basova <basovavalery@gmail.com>
2022-09-14 00:52:09 -0400

    add motion layout

--- app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsViewModel.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsViewModel.kt
@@ -26,10 +26,6 @@ class ElectionsViewModel(private val electionsRepository: ElectionsRepository) :
     val navigateToVoterInfo: LiveData<Election>
         get() = _navigateToVoterInfo
 
-    //TODO: Create live data val for saved elections
-
-    //TODO: Create val and functions to populate live data for upcoming elections from the API and saved elections from local database
-
     init {
         viewModelScope.launch {
             refreshUpcomingElections()
